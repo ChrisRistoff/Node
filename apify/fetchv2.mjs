0e95226f5109ebf28b697d483a455d363739ae5e
krasenHristov <doanusho@gmail.com>
2023-06-28 18:55:05 +0100

    added checks, can't get data on which products were missed and their prices but it is significantly faster

--- apify/fetchv2.mjs
+++ apify/fetchv2.mjs
@@ -2,11 +2,12 @@ import fetch from "node-fetch";
 
 const url = "http://localhost:8080/products";
 let products = {};
-let missedProductsPrices = [];
 let minPrice = 0;
 let maxPricePossible = 100000;
-let maxPrice = 1;
+let maxPrice = 501;
 let step = 1000;
+let maxCounter = 0;
+let missedPrices = [];
 
 async function fetchProducts(minPrice, maxPrice) {
   const res = await fetch(`${url}?minPrice=${minPrice}&maxPrice=${maxPrice}`);
@@ -19,11 +20,23 @@ async function getAllProductsLength() {
   return data.total;
 }
 
+function dictToArray(dict) {
+  let arr = [];
+  for (let key in dict) {
+    arr.push(dict[key]);
+  }
+  return arr;
+}
+
 async function scrapeProducts(minPrice, maxPrice) {
 
   if (maxPrice <= maxPricePossible) {
 
     let data = await fetchProducts(minPrice, maxPrice);
+
+    if (maxCounter > 2) {
+      return products;
+    }
 
     if (data.count > 1000) {
       step = step / 2;
@@ -31,26 +44,26 @@ async function scrapeProducts(minPrice, maxPrice) {
       return await scrapeProducts(minPrice, maxPrice);
     }
 
-    if (data.count < 500) {
-      step = step * 1.5;
-      return await scrapeProducts(minPrice, maxPrice + step);
-    }
-
-
-    for (let i = 0; i < data.count; i++) {
+   for (let i = 0; i < data.count; i++) {
         products[data.products[i].id] = data.products[i];
      }
 
     minPrice = maxPrice;
     maxPrice = maxPrice + step;
+
+    if (maxPrice > maxPricePossible) {
+      maxPrice = maxPricePossible;
+      maxCounter = maxCounter + 1;
+    }
     return await scrapeProducts(minPrice, maxPrice);
     }
 
-  return { products, missedProductsPrices };
+  return products;
 }
 
 let totalProducts = await getAllProductsLength();
-let data = await scrapeProducts(minPrice, maxPrice);
+let productsObj = await scrapeProducts(minPrice, maxPrice);
+let productsArray = dictToArray(productsObj);
 
 console.log(`Total products: ${totalProducts}`);
-console.log(`Total products scraped: ${Object.keys(data.products).length}`);
+console.log(`Total products scraped: ${productsArray.length}`);
